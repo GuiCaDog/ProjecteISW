25bea5e112fd27c7a9dabb7825448d3b4a88ce52
Guillem Calabuig Domenech <lvakakrie@hotmail.com>
2019-09-25 20:49:21 +0200

    Kiko's Life

--- ProyectoPracticas/ClassLibrary/Class1.cs
+++ ProyectoPracticas/ClassLibrary/Class1.cs
@@ -13,5 +13,10 @@ namespace ClassLibrary
         //...
         //Jo guicado també
         //for(int j = new j*10^24512){ <3 }
+        //public class Persona<E extends Kiko> { 
+        // private boolean Tostat,Existeix;
+        // public void Tostar(){Tostat=true;}
+        // public void Menjar(){Existeix=false;}
+        // public void SerKiko(){ while(Existeix) {try{if(Tostat){Menjar();} SerKiko();}catch(StackOVerFlowError e){Tostar();}}}
     }
 }
