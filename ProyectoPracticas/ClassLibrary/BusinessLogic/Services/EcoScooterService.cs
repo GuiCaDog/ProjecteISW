797f7496df6daadf1c2869e48702f9bba11e69b9
Guillem Calabuig Domenech <lvakakrie@hotmail.com>
2019-11-06 13:46:46 +0100

    hola

--- ProyectoPracticas/ClassLibrary/BusinessLogic/Services/EcoScooterService.cs
+++ ProyectoPracticas/ClassLibrary/BusinessLogic/Services/EcoScooterService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EcoScooter.Entities;
+using EcoScooter.Persistence;
+using EcoScooter.BusinessLogic.Services;
 
 namespace EcoScooter.BusinessLogic.Services
 {
-    class EcoScooterService
+    public class EcoScooterService : IEcoScooterService
     {
+        private readonly IDAL dal;
+        public EcoScooterService(IDAL dal)
+        {
+            this.dal = dal;
+        }
+
+        public void removeAllData()
+        {
+
+        }
     }
 }
