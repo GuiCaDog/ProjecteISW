7fe8c3f20094605f73d59a7367dd53d3998c5c36
gabi26 gabi26 <gabigcg26@hotmail.es>
2019-12-03 20:23:28 +0100

    Intent de css

--- ProyectoPracticas/GUIEcoScotter/LoginRegister/EcoScooterAppForm.Designer.cs
+++ ProyectoPracticas/GUIEcoScotter/LoginRegister/EcoScooterAppForm.Designer.cs
@@ -143,6 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1039, 596);
             this.Controls.Add(this.textoError);
             this.Controls.Add(this.tableLayoutPanel1);
